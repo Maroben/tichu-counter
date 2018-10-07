252b59cc4a1477277cfa17c79bb733f775f0bca1
Dijan Helbling <maroben@daishen.ch>
2018-10-07 22:49:10 +0200

    rounds

--- src/components/rounds.jsx
+++ src/components/rounds.jsx
@@ -3,29 +3,29 @@ import { instanceOf } from 'prop-types';
 import { withCookies, Cookies } from 'react-cookie';
 
 class Rounds extends Component {
-    static propTypes = {
-      cookies: instanceOf(Cookies).isRequired
-    };
-    constructor(props) {
-      super(props);
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired
+  };
+  constructor(props) {
+    super(props);
 
-      this.c = this.props.cookies;
-    }
-    
-    render() {
-      return (
-        this.c.get('rounds').map((round, index) => (
-          <div key={index} className="game">
-            <div className={`game-box ${round.teamA.big}`}></div>
-            <div className={`game-box ${round.teamA.small}`}></div>
-            <div className="game-box">{round.teamA.points}</div>
-            <div className="game-box">vs</div>
-            <div className="game-box">{round.teamB.points}</div>
-            <div className={`game-box ${round.teamB.small}`}></div>
-            <div className={`game-box ${round.teamB.big}`}></div>
-          </div>
-        ))
-      );
-    }
+    this.c = this.props.cookies;
+  }
+
+  render() {
+    return (
+      this.c.get('rounds').map((round, index) => (
+        <div key={index} className="game">
+          <div className={`game-box ${round.teamA.big}`}></div>
+          <div className={`game-box ${round.teamA.small}`}></div>
+          <div className="game-box">{round.teamA.points}</div>
+          <div className="game-box">vs</div>
+          <div className="game-box">{round.teamB.points}</div>
+          <div className={`game-box ${round.teamB.small}`}></div>
+          <div className={`game-box ${round.teamB.big}`}></div>
+        </div>
+      ))
+    );
+  }
 }
 export default withCookies(Rounds);
