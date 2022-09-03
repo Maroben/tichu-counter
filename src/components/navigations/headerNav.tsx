236caff8109641276249a6df6de810c8d9e90b54
roman.kramer <roman.kramer@ekt.ch>
2022-09-03 10:41:23 +0200

    Create a CSS class to show that we are gender neutral.

--- src/components/navigations/headerNav.tsx
+++ src/components/navigations/headerNav.tsx
@@ -13,6 +13,14 @@ const styles = (theme: Theme) =>
       flexGrow: 1,
       textDecoration: 'none',
     },
+    lgbtqBackground: {
+      background: `linear-gradient(180deg, #FE0000 16.66%,
+        #FD8C00 16.66%, 33.32%,
+        #FFE500 33.32%, 49.98%,
+        #119F0B 49.98%, 66.64%,
+        #0644B3 66.64%, 83.3%,
+        #C22EDC 83.3%)`,
+    },
   });
 
 interface Props extends WithStyles<typeof styles> {}
@@ -20,7 +28,7 @@ interface Props extends WithStyles<typeof styles> {}
 const HeaderNav = ({ classes: c }: Props) => {
   return (
     <AppBar position="sticky">
-      <Toolbar>
+      <Toolbar className={c.lgbtqBackground}>
         <Typography variant="h5" color="inherit" className={c.title} component={Link} to="/">
           Tichu Counter
         </Typography>
